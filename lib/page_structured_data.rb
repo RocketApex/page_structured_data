0f4dc390fb3437b534d3507a76375a4759d1bc33
Jey Geethan <485345+jeygeethan@users.noreply.github.com>
2023-10-31 20:44:57 +0530

    Require the essential files

--- lib/page_structured_data.rb
+++ lib/page_structured_data.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "page_structured_data/version"
+require_relative 'page_structured_data/anchors'
+require_relative 'page_structured_data/breadcrumbs'
+require_relative 'page_structured_data/meta'
+require_relative 'page_structured_data/page_types/news_article'
 
 module PageStructuredData
   class Error < StandardError; end
